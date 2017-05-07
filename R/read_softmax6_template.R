c76ec0ad663f2420b955f979cb45ff607c56daeb
Brian Connelly <bdc@bconnelly.net>
2017-05-06 19:45:02 -0700

    read_softmax6_template updates

--- R/read_softmax6_template.R
+++ R/read_softmax6_template.R
@@ -6,6 +6,13 @@
 #' not (\code{FALSE}) to include non-annotated wells (default: \code{FALSE})
 #' @param zeropad_wells Logical value indicating whether or not well names should
 #' be zero padded (e.g., "A04") (default: \code{FALSE})
+#' @param wellsAsFactors Logical value indicating whether or not well names
+#' (e.g., "H5") should be treated as factors (default: \code{TRUE})
+#' @param groupsAsFactors Logical value indicating whether or not groups
+#' should be treated as factors (default: \code{TRUE})
+#' @param typesAsFactors Logical value indicating whether or not types
+#' should be treated as factors (default: \code{TRUE})
+#' @inheritParams utils::read.table
 #' @param ... Additional parameters (not used)
 #'
 #' @return A data frame
@@ -18,6 +25,10 @@
 read_softmax6_template <- function(file,
                                    include_unspecified = FALSE,
                                    zeropad_wells = FALSE,
+                                   wellsAsFactors = TRUE,
+                                   groupsAsFactors = TRUE,
+                                   typesAsFactors = TRUE,
+                                   encoding = "UCS-2LE",
                                    ...) {
     d <- read.delim(
         file = file,
@@ -25,11 +36,11 @@ read_softmax6_template <- function(file,
         skip = 1,
         col.names = c(
             "Well",
-            "GroupName",
-            "GroupType",
-            "SampleName",
-            "Desc1Name",
-            "Desc1Value",
+            "Group",
+            "Type",
+            "SampleName", # Sample?
+            "Desc1Name",  # Desc1?
+            "Desc1Value", # Desc1Value
             "Desc1Units",
             "Desc2Name",
             "Desc2Value",
@@ -37,19 +48,27 @@ read_softmax6_template <- function(file,
             "HANGING"
         ),
         blank.lines.skip = TRUE,
-        fileEncoding = "UCS-2LE"
+        fileEncoding = encoding,
+        stringsAsFactors = FALSE
     )
 
     d <- subset(d, select = -HANGING)
 
     if (include_unspecified == FALSE) {
-        d <- d[d$GroupType != "" & d$Desc1Name != "", ]
+        d <- d[d$Type != "" & d$Desc1Name != "", ]
     }
 
     if (zeropad_wells == FALSE) {
-        # TODO: rename the wells
-        warning("zeropad_wells == FALSE not yet implemented")
+        d$Well <- paste0(
+            substr(d$Well, 1, 1),
+            as.integer(substr(d$Well, 2, 5))
+        )
     }
 
+    if (wellsAsFactors) d$Well <- forcats::as_factor(d$Well)
+    if (groupsAsFactors) d$Group <- forcats::as_factor(d$Group)
+    if (typesAsFactors) d$Type <- forcats::as_factor(d$Type)
+
+    class(d) <- c("data.frame", "softermaxTemplate")
     d
 }
