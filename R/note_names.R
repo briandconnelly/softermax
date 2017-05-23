407c9172ef5a9c8422be2c56f43318484ebf9a4b
Brian Connelly <bdc@bconnelly.net>
2017-05-23 13:57:13 -0700

    Added note_names helper

---
+++ R/note_names.R
@@ -0,0 +1,16 @@
+#' Return the names of all notes in an experiment
+#'
+#' @param x A \code{softermax.experiment} object containing information
+#' about an experiment in SoftMax Pro
+#'
+#' @return A vector of strings
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' d <- read_softmax_xml("myfile.xml")
+#' note_names(d$experiments[[1]])
+#' }
+note_names <- function(x) {
+    unname(sapply(X = x$notes, FUN = function(x) attr(x, "name")))
+}
